9728a1bbc300cb4c27f802e042849c46f6066b76
Osama Abuzaid <usmiosama@hotmail.com>
2017-05-20 12:25:47 +0300

    Jotain skriptaus7ta

--- Cyberfork/Assets/Scripts/Player.cs
+++ Cyberfork/Assets/Scripts/Player.cs
@@ -4,25 +4,25 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
-    public GameObject player;
-    public float thrust;
-    public float maxSpeed;
-    public float breakForce;
-    public float backSpeed;
-    public float rollingFriction;
-
-    public float turnPower = 10;
+    public float thrust = 50;
+    public float maxSpeed = 20;
+    public float breakForce = 50;
+    public float backSpeed = 10;
+    public float rollingFriction = 5;
     public float turningSpeed = 100;
 
     Rigidbody2D rb;
-
-    private float forwardFriction { get { return thrust / maxSpeed; } }
-    private float backwardFriction { get { return thrust / backSpeed; } }
+    ForkManager forks;
+    bool isLifting = false;
+    //GameObject lifted = null;
+    
+    float forwardFriction { get { return thrust / maxSpeed; } }
+    float backwardFriction { get { return thrust / backSpeed; } }
     // Use this for initialization
     void Start()
     {
-        rb = gameObject.GetComponent <Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
+        forks = transform.GetChild(0).GetComponent<ForkManager>();
     }
     // Update is called once per frame
     void Update()
@@ -32,16 +32,24 @@ public class Player : MonoBehaviour
 
     void UpdateInput()
     {
+        Movement();
+        Lift();
+    }
+
+    void Movement()
+    {
+
         if (Input.GetKey(KeyCode.W))
         {
+            Debug.Log("as");
             Thrust(rb, thrust, forwardFriction);
         }
 
         else if (Input.GetKey(KeyCode.S))
         {
-            if (Vector2.Dot(rb.velocity, transform.right) > 0)
+            if (Vector2.Dot(rb.velocity, transform.up) > 0)
             {
-                rb.AddForce(-breakForce * transform.right);
+                rb.AddForce(-breakForce * transform.up);
             }
             else Thrust(rb, -thrust, -backSpeed);
         }
@@ -53,19 +61,39 @@ public class Player : MonoBehaviour
 
         if (Input.GetKey(KeyCode.A))
         {
-            rb.angularVelocity = turnPower;
+            rb.angularVelocity = turningSpeed;
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            rb.angularVelocity = -turnPower;
+            rb.angularVelocity = -turningSpeed;
         }
         else rb.angularVelocity = 0;
 
-        rb.velocity = transform.right * Vector2.Dot(transform.right, rb.velocity);
+        rb.velocity = transform.up * Vector2.Dot(transform.up, rb.velocity);
+    }
+
+    void Lift()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!isLifting && forks.objectsOnFork.Count > 0)
+            {
+                Rigidbody2D otherRb = forks.objectsOnFork[0].GetComponent<Rigidbody2D>();
+                if(otherRb != null)
+                {
+                    FixedJoint2D joint = otherRb.gameObject.AddComponent<FixedJoint2D>();
+                    joint.connectedBody = rb;
+                }
+            }
+            else
+            {
+            }
+            isLifting = !isLifting;
+        }
     }
 
     void Thrust(Rigidbody2D rb, float thrust, float friction)
     {
-        rb.AddForce((thrust - friction * rb.velocity.magnitude) * transform.right);
+        rb.AddForce((thrust - friction * rb.velocity.magnitude) * transform.up);
     }
 }
