ea1e15708c1fb321a0c979b7c8ac30a7b1e10bd8
Osama Abuzaid <usmiosama@hotmail.com>
2017-05-21 10:31:16 +0300

    BoxSpawner fixed

--- Cyberfork/Assets/Scripts/BoxSpawner.cs
+++ Cyberfork/Assets/Scripts/BoxSpawner.cs
@@ -5,22 +5,35 @@ public class BoxSpawner : NetworkBehaviour {
 
 	public GameObject Box;
 	public int numBoxes;
+    public float distFromWalls;
 
-	public override void OnStartServer()
+    float width;
+    float height;
+    public override void OnStartServer()
 	{
+        ArenaBuilder arenaBuilder = FindObjectOfType<ArenaBuilder>();
+        width = arenaBuilder.width;
+        height = arenaBuilder.height;
 		for (int i=0; i < numBoxes; i++)
 		{
-			var pos = new Vector3(
-				Random.Range(1.0f, 8.0f),
-				Random.Range(4.0f, 7.0f),
-				Random.Range(1.0f, 8.0f)
-			);
+            Vector3 pos = new Vector3(Random.Range(distFromWalls+1, width-distFromWalls+1),
+                                        Random.Range(distFromWalls+1, height-distFromWalls+1), 0);
+            float rotation = Random.Range(0, 360);
+            GameObject box = Instantiate(Box, pos, Quaternion.identity);
+            box.transform.Rotate(0, 0, rotation);
+            Debug.Log("moi");
+            NetworkServer.Spawn(box);
+			//var pos = new Vector3(
+			//	Random.Range(1.0f, 8.0f),
+			//	Random.Range(4.0f, 7.0f),
+			//	Random.Range(1.0f, 8.0f)
+			//);
 
-			//var rotation = Quaternion.Euler( Random.Range(0,180), Random.Range(0,180), Random.Range(0,180));
-			var rotation = Quaternion.Euler(0, 0, 0);
+			////var rotation = Quaternion.Euler( Random.Range(0,180), Random.Range(0,180), Random.Range(0,180));
+			//var rotation = Quaternion.Euler(0, 0, 0);
 
-			var boxi = (GameObject)Instantiate(Box, pos, rotation);
-			NetworkServer.Spawn(boxi);
+			//var boxi = (GameObject)Instantiate(Box, pos, rotation);
+			//NetworkServer.Spawn(boxi);
 		}
 	}
 }
